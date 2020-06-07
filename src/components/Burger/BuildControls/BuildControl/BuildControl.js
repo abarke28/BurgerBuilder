d9ffbf06dc873e639608a49a3f599584d52b269d
Alex Barker <abarke28@gmail.com>
2020-06-07 11:00:57 -0700

    Plug-in BuildControls to app

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,8 +3,8 @@ import styles from './BuildControl.module.css';
 
 const buildControl = (props) => {
     return (
-        <div className={stles.BuildControl}>
-            <div className={styles.Label}>{label}</div>
+        <div className={styles.BuildControl}>
+            <div className={styles.Label}>{props.label}</div>
             <button className={styles.Less} onClick={props.less}>Less</button>
             <button className={styles.More} onClick={props.more}>More</button>
         </div>
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
 import styles from './BuildControls.module.css';
 
 const buildControls = (props) => {
+    const controls = [
+        {label: 'Salad', type: 'salad'},
+        {label: 'Bacon', type: 'bacon'},
+        {label: 'Cheese', type: 'cheese'},
+        {label: 'Meat', type: 'meat'}
+    ];
+
     return (
         <div className={styles.BuildControls}>
-
+            {controls.map(b => (
+                <BuildControl key={b.label} label={b.label} type={b.type} />
+             ))}
         </div>
     );
 }
