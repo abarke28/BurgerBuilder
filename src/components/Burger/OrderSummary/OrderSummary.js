fa6d0da434d6c13a664c82f660a71026d6036fed
Alex Barker <abarke28@gmail.com>
2020-06-08 10:25:15 -0700

    Convert Order Summary to class-based component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,27 +2,34 @@ import React from 'react';
 import Aux from '../../../hoc/Auxillery';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(key => {
+class OrderSummary extends React.Component {
+    componentDidUpdate() {
+        console.log('Order Summary Updated');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(key => {
             return (
                 <li key={key}>
-                    <span style={{ textTransform: 'capitalize' }}>{key}</span>: {props.ingredients[key]}
+                    <span style={{ textTransform: 'capitalize' }}>{key}</span>: {this.props.ingredients[key]}
                 </li>
-        )});
+            )
+        });
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicous burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Total Price: <strong>${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout</p>
-            <Button buttonStyle="Success" click={props.continue}>Continue</Button>
-            <Button buttonStyle="Danger" click={props.cancel}>Cancel</Button>
-        </Aux>
-    );
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicous burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Total Price: <strong>${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout</p>
+                <Button buttonStyle="Success" click={this.props.continue}>Continue</Button>
+                <Button buttonStyle="Danger" click={this.props.cancel}>Cancel</Button>
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
