df49e7522225bfcd893b40efe49dc23d03fc9ecd
Alex Barker <abarke28@gmail.com>
2020-06-08 09:01:01 -0700

    Add hamburger icon styling

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logoImage from '../../../../assets/images/burger-logo.png';
 
 import styles from './DrawerToggle.module.css';
 
 const drawerToggle = (props) => {
     return (
-        <button className={styles.DrawerToggle} onClick={props.click}>
-            <img src={logoImage} alt="="/>
-        </button>
+        <div className={styles.DrawerToggle} onClick={props.click}>
+            <div/>
+            <div/>
+            <div/>
+        </div>
      );
 }
 
