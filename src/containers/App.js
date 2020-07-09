d53036f89f2e835d8b940163831038e36c2e92b4
Alex Barker <abarke28@gmail.com>
2020-07-09 08:21:57 -0700

    Add CheckoutSummary

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
+import Checkout from './Checkout/Checkout';
 import styles from './App.module.css';
 
 function App() {
     return (
         <Layout className={styles.App}>
-            <BurgerBuilder/>
+            <BurgerBuilder />
+            <Checkout />
         </Layout>
     );
 }
